da4b05d39b038898a15ee12164540ddca71d4ee7
ahmed elnakeeb <ahmedelnakeeb2016@outlook.com>
2022-05-28 15:16:18 +0200

    remove pandas

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI , Path
-import pandas as pd
 from typing import Optional
 import json
 
 
 app=FastAPI()
-df=pd.read_csv("k.csv",encoding="utf-8")
 with open('finale.json',encoding="utf_8",mode="r") as json_file:
     json_ = json.load(json_file)
 
@@ -26,17 +24,17 @@ def ltr(letter:str=Path(None,description="arabic letter")):
     except:
         return ("did not work sorry hazem")
 
-@app.get("/search}")
+@app.get("/search")
 def search(last:Optional[str]=None,b_last:Optional[str]=None,count:Optional[int]=None):
-        result=[]
-        if last and (not count) and (not b_last ):
-            return ltr(last)
-        elif b_last and last and (not count):
-            return df.loc[ (df['b_last'] ==b_last) & (df['last'] ==last) ]["words"]
-        elif count and last and b_last:
-            return df.loc[ (df['b_last'] ==b_last) & (df['last'] ==last)&(df['count'] ==count)   ]["words"]
-        elif count and last:
-             return df.loc[(df['last'] ==last)&(df['count'] ==count)   ]["words"]
-        elif count:
-            return df.loc[(df['count'] ==count)   ]["words"]
-        return {"results":"sothing went wrong "}
+    try:
+        if last:
+            res=json_[last]
+            if count:
+                res=[i for i in res if len(i)==int(count)]
+            if b_last:
+                res=[i for i in res if i[-2]==b_last]
+            return {"results":res}
+        
+    except:
+        pass
+    return {"results":"sothing went wrong"}
